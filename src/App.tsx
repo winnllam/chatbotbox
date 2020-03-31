170ee96064aaf963918444d9df2c9f7da3e13429
Winn <winnieee.lam00@gmail.com>
2020-03-30 21:37:06 -0400

    Fixed alignment

--- src/App.tsx
+++ src/App.tsx
@@ -68,11 +68,6 @@ const App: React.FC = () => {
   return (
     <div className={styles.page}>
       <div className={styles.container}>
-        <header className={styles.title}>
-          <h3>
-            <code>Chat Time</code>
-          </h3>
-        </header>
         <div className={styles.chatBox}>
           {messages.map((m: any) => (
             <ChatMessage key={m.time.valueOf()} name={m.id} time={m.time}>
